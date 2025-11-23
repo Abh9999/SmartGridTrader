4c9489ddd5135d764b99460cfe4c90d914c2de77
Abhishek Hirpara <abhihirpara9999@gmail.com>
2025-11-23 10:31:05 +0100

    Final AI Model (RandomForest) and MAE Evaluation

--- src/ai_brain.py
+++ src/ai_brain.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 #from sklearn.linear_model import LinearRegression
 
 # --- 1. DATA LOADING AND UNNESTING ---
@@ -53,8 +54,11 @@ model.fit(X_train, y_train)
 #print("Training Linear Regressor...")
 #model.fit(X_train, y_train)
 
-# Evaluation
-score = model.score(X_test, y_test)
+# Model Evaluation
+score = model.score(X_test, y_test) # R^2 Score
+y_pred = model.predict(X_test)
+mae = mean_absolute_error(y_test, y_pred)
 
 print("\n--- Training Complete ---")
-print(f"Model R^2 Score (Accuracy): {score:.4f}")
+print(f"Model R^2 Score: {score:.4f}")
+print(f"Model MAE (Avg. Price Error): {mae:.4f} Cents/kWh")
